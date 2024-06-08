f103660122eae83d144a4f4964171ea8dec160a8
Roberto Sena <92528737+robertosena@users.noreply.github.com>
2024-06-08 18:44:59 +0200

    *

---
+++ DATA/MVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace $safeprojectname$.Data
+{
+    public class MVCContext : DbContext
+    {
+        public MVCContext (DbContextOptions<MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
